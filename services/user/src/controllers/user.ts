15d4c286fc1fb9b811c2519818389074f5b83611
YashChauhan7060 <yc230310@gmail.com>
2025-12-15 15:24:27 +0530

    feat(upload): add Multer + DataURI Cloudinary integration to user service

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -3,6 +3,9 @@ import User from "../model/User.js";
 import jwt from "jsonwebtoken";
 import TryCatch from "../utils/TryCatch.js";
 import { AuthenticatedRequest } from "../middleware/isAuth.js";
+import getBuffer from "../utils/dataUri.js";
+import { v2 as cloudinary } from "cloudinary";
+
 
 export const loginUser = TryCatch(async (req: Request, res: Response) => {
   const { email, name, image } = req.body;
@@ -46,3 +49,72 @@ export const getUserProfile = TryCatch(async (req: Request, res: Response) => {
 
   res.json(user);
 });
+
+export const updateUser = TryCatch(async (req: AuthenticatedRequest, res: Response) => {
+  const { name, instagram, facebook, linkedin, bio } = req.body;
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      name,
+      instagram,
+      facebook,
+      linkedin,
+      bio,
+    },
+    { new: true }
+  );
+
+  const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+    expiresIn: "5d",
+  });
+
+  res.json({
+    message:"user updated",
+    token,
+    user
+  });
+});
+
+export const updateProfilePic = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    const file = req.file;
+
+    if (!file) {
+      res.status(400).json({
+        message: "No file to upload",
+      });
+      return;
+    }
+
+    const fileBuffer = getBuffer(file);
+
+    if (!fileBuffer || !fileBuffer.content) {
+      res.status(400).json({
+        message: "Failed to generate buffer",
+      });
+      return;
+    }
+    const cloud = await cloudinary.uploader.upload(fileBuffer.content, {
+      folder: "blogs",
+    });
+
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+        image: cloud.secure_url,
+      },
+      { new: true }
+    );
+
+    const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+      expiresIn: "5d",
+    });
+
+    res.json({
+      message: "User Profile pic updated",
+      token,
+      user,
+    });
+  }
+);
--- services/user/src/server.ts
+++ services/user/src/server.ts
@@ -2,8 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./utils/db.js";
 import userRoutes from "./routes/user.js";
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api_Key,
+  api_secret: process.env.Cloud_Api_Secret,
+});
 
 const app = express();
 
