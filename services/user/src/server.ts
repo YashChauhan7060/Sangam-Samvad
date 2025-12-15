2bcb5934688128d794dc9913bf8153e04ecea10c
YashChauhan7060 <yc230310@gmail.com>
2025-12-15 12:17:00 +0530

    Start creating user service

---
+++ services/user/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./utils/db.js";
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDb();
+
+app.use("/api/v1", userRoutes);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
