cae175b99adff802536735a4fc26e0e52c06ae82
AGNIHOTRIMAHI <mahiagnihotri256@gmail.com>
2025-12-17 19:12:22 +0530

    saving latest changes

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -1,70 +1,14 @@
-// import { sql } from "../utils/db";
-// import TryCatch from "../utils/TryCatch";
-// import axios from "axios"; 
+import { sql } from "../utils/db";
+import TryCatch from "../utils/TryCatch";
+import axios from "axios"; 
 
-// export const getAllBlogs = TryCatch(async (req, res) => {
-//   const { searchQuery = "", category = "" } = req.query;
-
-
-//   let blogs;
-
-//   if (searchQuery && category) {
-//     blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
-//       "%" + searchQuery + "%"
-//     } OR description ILIKE ${
-//       "%" + searchQuery + "%"
-//     }) AND category = ${category} ORDER BY create_at DESC`;
-//   } else if (searchQuery) {
-//     blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
-//       "%" + searchQuery + "%"
-//     } OR description ILIKE ${"%" + searchQuery + "%"}) ORDER BY create_at DESC`;
-//   } else if (category) {
-//     blogs =
-//       await sql`SELECT * FROM blogs WHERE category=${category} ORDER BY create_at DESC`;
-//   } else {
-//     blogs = await sql`SELECT * FROM blogs ORDER BY create_at DESC`;
-//   }
-
-//   console.log("Serving from db");
+export const getAllBlogs = TryCatch(async (req, res) => {
+  const { searchQuery = "", category = "" } = req.query;
 
 
-//   res.json(blogs);
-// });
+  let blogs;
 
-
-// export const getSingleBlog = TryCatch(async (req, res) => {
-//   const blogid = req.params.id;
-
-
-
-//   const blog = await sql`SELECT * FROM blogs WHERE id = ${blogid}`;
-
-//   if (blog.length === 0) {
-//     res.status(404).json({
-//       message: "no blog with this id",
-//     });
-//     return;
-//   }
-
-//   const { data } = await axios.get(
-//     `${process.env.USER_SERVICE}/api/v1/user/${blog[0].author}`
-//   );
-
-//   const responseData = { blog: blog[0], author: data };
-
-
-//   res.json(responseData);
-// });
-
-
-import {sql} from "../utils/db.js";
-import TryCatch from "../utils/TryCatch.js";
-import axios from 'axios';
-export const getAllBlogs=TryCatch(async(req,res)=>{
-     const { searchQuery, category } = req.query;
-     let blogs 
-      console.log(searchQuery); 
-     if (searchQuery && category) {
+  if (searchQuery && category) {
     blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
       "%" + searchQuery + "%"
     } OR description ILIKE ${
@@ -74,23 +18,40 @@ export const getAllBlogs=TryCatch(async(req,res)=>{
     blogs = await sql`SELECT * FROM blogs WHERE (title ILIKE ${
       "%" + searchQuery + "%"
     } OR description ILIKE ${"%" + searchQuery + "%"}) ORDER BY create_at DESC`;
-  
+  } else if (category) {
+    blogs =
+      await sql`SELECT * FROM blogs WHERE category=${category} ORDER BY create_at DESC`;
   } else {
     blogs = await sql`SELECT * FROM blogs ORDER BY create_at DESC`;
   }
-    // blogs =await sql `SELECT * FROM blogs ORDER BY create_at DESC`;
-     res.json(blogs);
 
+  console.log("Serving from db");
+
+
+  res.json(blogs);
 });
 
 
 export const getSingleBlog = TryCatch(async (req, res) => {
-    
-  const blog = await sql`SELECT * FROM blogs WHERE id = ${req.params.id}`;
+  const blogid = req.params.id;
+
+
+
+  const blog = await sql`SELECT * FROM blogs WHERE id = ${blogid}`;
+
+  if (blog.length === 0) {
+    res.status(404).json({
+      message: "no blog with this id",
+    });
+    return;
+  }
+
   const { data } = await axios.get(
     `${process.env.USER_SERVICE}/api/v1/user/${blog[0].author}`
   );
 
   const responseData = { blog: blog[0], author: data };
+
+
   res.json(responseData);
 });
